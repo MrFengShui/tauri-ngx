9460989f0f21fab7bda7cbd5e55b4e261e66ec63
MrFengShui <luansongjian@gmail.com>
2024-09-19 11:44:31 +0800

    添加drawio文件

--- src/app/algorithm/sort/service/pairwise-sort.service.ts
+++ src/app/algorithm/sort/service/pairwise-sort.service.ts
@@ -131,48 +131,7 @@ export class RecursivePairwiseNetworkingSortService extends AbstractBinarySortSe
 
 @Injectable()
 export class IterativePairwiseNetworkingSortService extends RecursivePairwiseNetworkingSortService {
-    // int a = 1;
-    // int b = 0;
-    // int c = 0;
-    // int d = 0;
-    // int e = 0;
-    // while (a < length){
-    //     b = a;
-    //     c = 0;
-    //     while (b < length){
-    //         if(Reads.compareIndices(array, b - a, b, sleep, true) == 1) {
-    //             Writes.swap(array, b - a, b, sleep, true, false);
-    //         }
-    //         c = (c + 1) % a;
-    //         b++;
-    //         if (c == 0){
-    //             b += a;
-    //         }
-    //     }
-    //     a *= 2;
-    // }
-    // a /= 4;
-    // e = 1;
-    // while (a > 0){
-    //     d = e;
-    //     while (d > 0){
-    //         b = ((d + 1) * a);
-    //         c = 0;
-    //         while (b < length){
-    //             if(Reads.compareIndices(array, b - (d * a), b, sleep, true) == 1) {
-    //                 Writes.swap(array, b - (d * a), b, sleep, true, false);
-    //             }
-    //             c = (c + 1) % a;
-    //             b++;
-    //             if (c == 0){
-    //                 b += a;
-    //             }
-    //         }
-    //         d /= 2;
-    //     }
-    //     a /= 2;
-    //     e = (e * 2) + 1;
-    // }
+
     protected override async sortByAscent(source: SortDataModel[], lhs: number, rhs: number, option: string | number | undefined, callback: (param: SortStateModel) => void): Promise<void> {
         let j: number, level: number, index: number = -1, times: number = 0;
 
